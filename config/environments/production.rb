75babcd6ee9478b83978e59d6ed579f4977b42ad
Ahmed Sha3ban Amer <48615086+Sha3booony@users.noreply.github.com>
2026-01-09 19:17:05 +0200

    Allow Requests hosts

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,11 +76,9 @@ Rails.application.configure do
   config.active_record.attributes_for_inspect = [ :id ]
 
   # Enable DNS rebinding protection and other `Host` header attacks.
-  # config.hosts = [
-  #   "example.com",     # Allow requests from example.com
-  #   /.*\.example\.com/ # Allow requests from subdomains like `www.example.com`
-  # ]
-  #
+  config.hosts << "backend.mila-knight.com"
+  config.hosts << /.*\.mila-knight\.com/ # Allow requests from all subdomains
+  
   # Skip DNS rebinding protection for the default health check endpoint.
-  # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
+  config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
 end
